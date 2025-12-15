18325c76829686edfc5468257059939e01b92f89
ShotaKhakhishvili <shotakhakhishvili@gmail.com>
2025-12-15 19:34:11 +0400

    Setup for 3D

--- src/Private/main.cpp
+++ src/Private/main.cpp
@@ -1,5 +1,10 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+
+#include <glm\glm.hpp>
+#include <glm\gtx\matrix_transform_2d.hpp>
+#include <glm\gtc\type_ptr.hpp>
+
 #include <iostream>
 
 #include "Path.h"
@@ -87,14 +92,33 @@ int main() {
 
     Texture texture = Texture("1.png", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
 
+    texture.TexUnit(shader, "tex0", 0);
+
     while (!glfwWindowShouldClose(window)) {
         glfwPollEvents();
         glClear(GL_COLOR_BUFFER_BIT);
         // Draw Triangle ------------------------------------
         shader.Activate();
+
+        // setup for 3D
+        glm::mat4 model = glm::mat4(1.0f);
+        glm::mat4 view = glm::mat4(1.0f);
+        glm::mat4 proj = glm::mat4(1.0f);
+
+        //model = glm::rotate(model, glm::radians(45.0f), glm::vec3(0, 0, 1));
+        view = glm::translate(view, glm::vec3(0.0f, 0.0f, -2.0f));
+        proj = glm::perspective(glm::radians(45.0f), (float)width / (float)height, 0.1f, 100.0f);
+
+        int modelLoc = glGetUniformLocation(shader.getID(), "model");
+        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+        int viewLoc = glGetUniformLocation(shader.getID(), "view");
+        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+        int projLoc = glGetUniformLocation(shader.getID(), "proj");
+        glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(proj));
+
         glBindTexture(GL_TEXTURE_2D, texture.getID());
         vao.Bind();
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(int), GL_UNSIGNED_INT, 0);
 
         glfwSwapBuffers(window);
     }
