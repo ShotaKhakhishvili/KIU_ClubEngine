1421bb79bc591d416741c7e15de601f3a7fb1bdd
Giorgi K. <giorgi.kveliashvili07@gmail.com>
2025-12-25 02:09:37 +0400

    Update Sublevel.cpp

--- Game/src/Private/Sublevel.cpp
+++ Game/src/Private/Sublevel.cpp
@@ -3,54 +3,54 @@
 std::vector<std::vector<Placeholderinfo>> Sublevel::subLevels{
         {
             {ObjectType::Barrier, ObjectType::SABook, ObjectType::Barrier, {0,0,0}},
-            {ObjectType::SABook, ObjectType::Barrier, ObjectType::Barrier, {5,0,0}},
-            {ObjectType::Barrier, ObjectType::Barrier, ObjectType::SABook, {10,0,0}}
+            {ObjectType::SABook, ObjectType::Barrier, ObjectType::Barrier, {10,0,0}},
+            {ObjectType::Barrier, ObjectType::Barrier, ObjectType::SABook, {20,0,0}}
         },
         {
-            {ObjectType::Empty, ObjectType::SABook, ObjectType::Empty, {3,0,0}},
-            {ObjectType::Empty, ObjectType::Barrier, ObjectType::Empty, {5,0,0}},
-            {ObjectType::Empty, ObjectType::SABook, ObjectType::Empty, {5,2,0}},
-            {ObjectType::Empty, ObjectType::SABook, ObjectType::Empty, {7,0,0}}
+            {ObjectType::Empty, ObjectType::SABook, ObjectType::Empty, {6,0,0}},
+            {ObjectType::Empty, ObjectType::Barrier, ObjectType::Empty, {10,0,0}},
+            {ObjectType::Empty, ObjectType::SABook, ObjectType::Empty, {10,2,0}},
+            {ObjectType::Empty, ObjectType::SABook, ObjectType::Empty, {14,0,0}}
         },
     {
-            {ObjectType::SABook, ObjectType::Empty, ObjectType::SABook, {3,0,0}},
-            {ObjectType::Barrier, ObjectType::SABook, ObjectType::Barrier, {5,0,0}},
-            {ObjectType::SABook, ObjectType::Empty, ObjectType::SABook, {5,2,0}},
-            {ObjectType::SABook, ObjectType::Empty, ObjectType::SABook, {7,0,0}}
+            {ObjectType::SABook, ObjectType::Empty, ObjectType::SABook, {6,0,0}},
+            {ObjectType::Barrier, ObjectType::SABook, ObjectType::Barrier, {10,0,0}},
+            {ObjectType::SABook, ObjectType::Empty, ObjectType::SABook, {10,2,0}},
+            {ObjectType::SABook, ObjectType::Empty, ObjectType::SABook, {14,0,0}}
         },
     {
             {ObjectType::Empty, ObjectType::SABook, ObjectType::Empty, {0,0,0}},
-            {ObjectType::Empty, ObjectType::SABook, ObjectType::Empty, {2.5,0,0}},
-            {ObjectType::Empty, ObjectType::Barrier, ObjectType::Empty, {5,0,0}},
-            {ObjectType::Empty, ObjectType::SABook, ObjectType::Empty, {5,2,0}},
-            {ObjectType::Empty, ObjectType::SABook, ObjectType::Empty, {7.5,0,0}},
-            {ObjectType::Empty, ObjectType::SABook, ObjectType::Empty, {10,0,0}}
+            {ObjectType::Empty, ObjectType::SABook, ObjectType::Empty, {5,0,0}},
+            {ObjectType::Empty, ObjectType::Barrier, ObjectType::Empty, {10,0,0}},
+            {ObjectType::Empty, ObjectType::SABook, ObjectType::Empty, {10,2,0}},
+            {ObjectType::Empty, ObjectType::SABook, ObjectType::Empty, {15,0,0}},
+            {ObjectType::Empty, ObjectType::SABook, ObjectType::Empty, {20,0,0}}
         },
     {
             {ObjectType::SABook, ObjectType::Empty, ObjectType::SABook, {0,0,0}},
-            {ObjectType::Barrier, ObjectType::SABook, ObjectType::Barrier, {2.5,0,0}},
-            {ObjectType::SABook, ObjectType::Empty, ObjectType::SABook, {2.5,2,0}},
-            {ObjectType::SABook, ObjectType::Barrier, ObjectType::SABook, {5,0,0}},
-            {ObjectType::Empty, ObjectType::SABook, ObjectType::Empty, {5,2,0}},
-            {ObjectType::Barrier, ObjectType::SABook, ObjectType::Barrier, {7.5,0,0}},
-            {ObjectType::SABook, ObjectType::Empty, ObjectType::SABook, {7.5,2,0}},
-            {ObjectType::SABook, ObjectType::Empty, ObjectType::SABook, {10,0,0}}
+            {ObjectType::Barrier, ObjectType::SABook, ObjectType::Barrier, {5,0,0}},
+            {ObjectType::SABook, ObjectType::Empty, ObjectType::SABook, {5,2,0}},
+            {ObjectType::SABook, ObjectType::Barrier, ObjectType::SABook, {10,0,0}},
+            {ObjectType::Empty, ObjectType::SABook, ObjectType::Empty, {10,2,0}},
+            {ObjectType::Barrier, ObjectType::SABook, ObjectType::Barrier, {15,0,0}},
+            {ObjectType::SABook, ObjectType::Empty, ObjectType::SABook, {15,2,0}},
+            {ObjectType::SABook, ObjectType::Empty, ObjectType::SABook, {20,0,0}}
         },
         {
             {ObjectType::SABook, ObjectType::Empty, ObjectType::Empty, {0,0,0}},
-            {ObjectType::Barrier, ObjectType::SABook, ObjectType::Empty, {2.5,0,0}},
-            {ObjectType::SABook, ObjectType::Empty, ObjectType::Empty, {2.5,2,0}},
-            {ObjectType::SABook, ObjectType::Barrier, ObjectType::SABook, {5,0,0}},
-            {ObjectType::Empty, ObjectType::SABook, ObjectType::Empty, {5,2,0}},
-            {ObjectType::Empty, ObjectType::SABook, ObjectType::Barrier, {7.5,0,0}},
-            {ObjectType::Empty, ObjectType::Empty, ObjectType::SABook, {7.5,2,0}},
-            {ObjectType::Empty, ObjectType::Empty, ObjectType::SABook, {10,0,0}}
+            {ObjectType::Barrier, ObjectType::SABook, ObjectType::Empty, {5,0,0}},
+            {ObjectType::SABook, ObjectType::Empty, ObjectType::Empty, {5,2,0}},
+            {ObjectType::SABook, ObjectType::Barrier, ObjectType::SABook, {10,0,0}},
+            {ObjectType::Empty, ObjectType::SABook, ObjectType::Empty, {10,2,0}},
+            {ObjectType::Empty, ObjectType::SABook, ObjectType::Barrier, {15,0,0}},
+            {ObjectType::Empty, ObjectType::Empty, ObjectType::SABook, {15,2,0}},
+            {ObjectType::Empty, ObjectType::Empty, ObjectType::SABook, {20,0,0}}
         }
     };
 
 Sublevel::Sublevel(std::vector<Placeholderinfo> infos, Player* player, glm::vec3 position) {
     for (int i = 0; i < infos.size(); i++) {
-        Placeholder* newPlaceHolder = new Placeholder(infos[i].a, infos[i].b, infos[i].c, infos[i].position, player);
+        Placeholder* newPlaceHolder = new Placeholder(infos[i].a, infos[i].b, infos[i].c, position + infos[i].position, player);
         Placeholders.push_back(newPlaceHolder);
     }
     this->size = infos.back().position.x;
